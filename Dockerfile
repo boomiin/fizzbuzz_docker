17b2feef0f35a9c63f21eb10b7866befe45843d1
Chris Aka <akachris225@gmail.com>
2021-12-10 16:30:27 +0100

    done

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ FROM ubuntu:18.04
 RUN apt update
 RUN apt -y install nodejs
 RUN apt -y install npm
-RUN rm -rf /var/lib/apt/lists/*
-
-CMD node fizzbuzz.js
+RUN mkdir /usr/local/workdir
+COPY fizzbuzz.js /usr/local/workdir
+WORKDIR /usr/local/workdir
--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,12 +6,12 @@ const rl = readline.createInterface({
 
 rl.question("Entrez un nombre : ", function(num){
     num = parseInt(num)
-    if(num % 5 == 0) console.log("Fizz")
+    if((num % 5 == 0) && (num % 7 == 0)) console.log("Fizz Buzz")
+    else if(num % 5 == 0) console.log("Fizz")
     else if(num % 7 == 0) console.log("Buzz")
-    else if((num % 5 == 0) && (num % 7 == 0)) console.log("Fizz Buzz")
     else console.log(num)
     rl.close();
-})
+});
 
 // var num = rl.prompt("Entrez un nombre : ")
 // num = parseInt(num)
