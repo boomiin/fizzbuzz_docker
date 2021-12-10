14ed69c5656cdc320b94459dac7cdd7cb7dfc01e
Chris Aka <akachris225@gmail.com>
2021-12-10 14:44:14 +0100

    fix else output

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -9,7 +9,7 @@ rl.question("Entrez un nombre : ", function(num){
     if(num % 5 == 0) console.log("Fizz")
     else if(num % 7 == 0) console.log("Buzz")
     else if((num % 5 == 0) && (num % 7 == 0)) console.log("Fizz Buzz")
-    else console.log("Unknown")
+    else console.log(num)
     rl.close();
 })
 
